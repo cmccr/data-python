6f61b988a79247bb7e39a14b9499788973f86d40
cmccr <cmccrory548@gmail.com>
2022-03-31 14:19:05 -0600

    finished up

--- data_presenter.py
+++ data_presenter.py
@@ -20,3 +20,18 @@ for row in cupcake_file:
 print(total)
 
 cupcake_file.close()
+
+import matplotlib.pyplot as plt
+
+x = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"]
+y = [10,40,32,84,60,52,18]
+
+plt.plot(x,y)
+
+plt.xlabel('Day Purchased')
+
+plt.ylabel('Cupcakes Purchased')
+
+plt.title('My Cupcake Sales')
+
+plt.show()
